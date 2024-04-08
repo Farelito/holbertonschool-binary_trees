00fa1c0c1ace60864ddbb174f74c5ce33746480e
GuerinTifenn <8807@holbertonstudents.com>
2024-04-08 09:30:21 +0000

    add binary_tree_print.c and modified binary_trees.h

--- binary_trees.h
+++ binary_trees.h
@@ -1,6 +1,7 @@
-#ifndef BINARY_TREES_H
-#define BINARY_TREES_H
+#ifndef __BINARY_TREES_H__
+#define __BINARY_TREES_H__
 
+#include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -25,6 +26,8 @@ typedef struct binary_tree_s bst_t;
 typedef struct binary_tree_s avl_t;
 typedef struct binary_tree_s heap_t;
 
+void binary_tree_print(const binary_tree_t *);
+
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
@@ -44,3 +47,5 @@ int binary_tree_is_full(const binary_tree_t *tree);
 int binary_tree_is_perfect(const binary_tree_t *tree);
 binary_tree_t *binary_tree_sibling(binary_tree_t *node);
 binary_tree_t *binary_tree_uncle(binary_tree_t *node);
+
+#endif
