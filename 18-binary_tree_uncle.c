27277f8e7fb77341613200e9c8eefd18a701195a
Farelito <bakoufarel@gmail.com>
2024-04-10 11:46:55 +0000

    task 18

---
+++ 18-binary_tree_uncle.c
@@ -0,0 +1,20 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_is_perfect - checks if a binary tree is perfect
+ * @tree: pointer to the root node of the tree to check
+ * Return: 1 if is perfect tree 0 if not
+ */
+binary_tree_t *binary_tree_uncle(binary_tree_t *node)
+{
+    if (node == NULL || node->parent == NULL)
+	    return (NULL);
+
+    if (node->parent->parent == NULL)
+        return (NULL);
+
+    if (node->parent->parent->left == node->parent)
+	    return (node->parent->parent->right);
+
+    return (node->parent->parent->left);
+}
